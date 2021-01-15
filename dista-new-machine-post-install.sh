a37fe3350646290bf70560d2ccf13d6289a15089
Andrei <andrei.belkin.0@protonmail.com>
2021-01-15 13:58:50 -0500

    Add post-install.sh for new machines

---
+++ dista-new-machine-post-install.sh
@@ -0,0 +1,68 @@
+clear
+echo
+echo "# ========== FETCHING ALL UPDATES =========="
+echo
+sudo apt update
+echo
+echo "# ============= UPDATES FETCHED ============"
+echo
+echo "# =========== INSTALLING UPDATES ==========="
+echo
+sudo apt upgrade -y
+echo
+echo "# ============ UPDATES INSTALLED ==========="
+echo
+echo "# ==== AUTOREMOVING UNNECESSARY PACKAGES ==="
+echo
+sudo apt autoremove -y
+echo
+echo "# ====== UNNECESSARY PACKAGES REMOVED ======"
+echo
+echo "# ======= INSTALLING BASIC PACKAGES ========"
+echo "# |  vim                                   |"
+echo "# |  nano                                  |"
+echo "# |  git                                   |"
+echo "# |  ansible                               |"
+echo "# =========================================="
+echo
+sudo apt install -y vim nano git ansible
+echo
+echo "# ======== BASIC PACKAGES INSTALLED ========"
+echo
+echo "# ====== RUNNING DISTA ANSIBLE CONFIG ======"
+echo
+ansible-pull -U https://github.com/andrei-belkin/dista-maintenance.git addDistaUser.yml
+echo
+echo "# ====== FINISHED DISTA ANSIBLE CONFIG ====="
+echo
+echo
+echo
+echo "# =========== SUMMARY OF CHANGES ==========="
+echo "# |  WARNING: Some steps may have been     |"
+echo "# |           skipped because they were    |"
+echo "# |           not necessary.               |"
+echo "# |                                        |"
+echo "# |  1. Fetched updates (apt update)       |"
+echo "# |                                        |"
+echo "# |  2. Applied updates (apt upgrade)      |"
+echo "# |                                        |"
+echo "# |  3. Auto-removed unnecessary           |"
+echo "# |     packages (apt autoremove)          |"
+echo "# |                                        |"
+echo "# |  4. Installed basic necessary          |"
+echo "# |     packages (apt install) :           |"
+echo "# |                                        |"
+echo "# |   - ansible                            |"
+echo "# |   - git                                |"
+echo "# |   - nano                               |"
+echo "# |   - vim                                |"
+echo "# |                                        |"
+echo "# |  5. Configured the system using the    |"
+echo "# |     Dista Ansible config :             |"
+echo "# |                                        |"
+echo "# |   - created dista-dev user             |"
+echo "# |                                        |"
+echo "# |  6. Done.                              |"
+echo "# =========================================="
+echo 
+echo "# ############## END OF SCRIPT #############"
